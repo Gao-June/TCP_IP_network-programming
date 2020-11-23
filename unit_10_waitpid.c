d6db12e2f7ca333cf61df889b1050dc9396bc195
Gao-June <53166605+Gao-June@users.noreply.github.com>
2020-11-24 00:37:59 +0800

    Create unit_10_waitpid.c

---
+++ unit_10_waitpid.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+    int status;
+    pid_t pid;
+
+    pid = fork();
+
+    if(pid == 0){
+		sleep(5);
+    	return 5;
+    }
+	else{
+		while(!waitpid(-1,&status,WNOHANG)){
+	    sleep(1);
+	    puts("sleep 1s");
+	}
+
+	if(WIFEXITED(status)){
+	    printf("child send %d \n",WEXITSTATUS(status));
+	}
+    }
+
+    return 0;
+}
