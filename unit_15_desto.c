4ff4898771df55520fe7333433d9123a2e38ad0c
Gao-June <53166605+Gao-June@users.noreply.github.com>
2020-12-03 21:19:52 +0800

    Create unit_15_desto.c

---
+++ unit_15_desto.c
@@ -0,0 +1,21 @@
+//练习使用 fdopen()
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void){
+	FILE *fp;
+	int fd = open("data.dat", O_WRONLY | O_CREAT | O_TRUNC);
+	if( fd == -1 ){
+		fputs("file open error!\n", stdout);
+		return -1;
+	}
+	
+	fp = fdopen(fd, "w");
+	fputs("Network C programming \n ", fp);
+	
+	fclose(fp);
+	
+	
+	return 0;
+}
